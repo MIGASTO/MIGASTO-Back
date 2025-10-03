93ef4fe309c91a1d846897347f11284fa3e8c900
Milton-Luna <miltonluna.m.16@gmail.com>
2025-10-03 16:35:11 -0500

    Configuracion, variables de entorno-Api-BD

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Pipes globales para validaciones
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
+  );
+
+ 
+  app.setGlobalPrefix('api');
+
+ 
+  app.enableCors({
+    origin: 'http://localhost:4200', 
+  });
+
+  const port = process.env.SERVER_PORT || 3000;
+  await app.listen(port);
+
+  console.log(`🚀 Server running on http://localhost:${port}/api`);
 }
 bootstrap();
