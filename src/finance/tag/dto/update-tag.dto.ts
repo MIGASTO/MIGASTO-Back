7ac4576ff3d1a2b9726f548ecee09706feaff412
Milton-Luna <miltonluna.m.16@gmail.com>
2025-12-01 12:19:30 -0500

    cambio dto

--- src/finance/tag/dto/update-tag.dto.ts
+++ src/finance/tag/dto/update-tag.dto.ts
@@ -5,10 +5,7 @@ import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
 export class UpdateTagDto extends PartialType(CreateTagDto) {
     @IsNotEmpty({ message: 'El nombre del tag es obligatorio.' })
       @IsString({ message: 'El nombre debe ser una cadena de texto.' })
-      @MaxLength(50, { message: 'El nombre no puede tener más de 50 caracteres.' })
-      @Matches(/^[a-zA-ZÀ-ÿ\u00f1\u00d1\s]+$/, {
-        message: 'El nombre solo puede contener letras y espacios.',
-      })
+      @MaxLength(30, { message: 'El nombre no puede tener más de 30 caracteres.' })
       nombre: string;
 }
 
